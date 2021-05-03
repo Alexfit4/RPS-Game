de02b385c6a3aaefbbec377f6969036915d453ff
Amir <alex.ashtiany@gmail.com>
2021-05-03 10:55:25 -0400

    created buttons and added ui design

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const results = document.getElementById("results");
 
 const computerPlay = () => {
-    let choices = ['Rock', 'Paper', 'Scissors']
+  let choices = ["Rock", "Paper", "Scissors"];
 
-    var item = choices[Math.floor(Math.random() * choices.length)];
+  var item = choices[Math.floor(Math.random() * choices.length)];
 
-    return item
-}
+  return item;
+};
 
-let userScore = 0
-let computerScore = 0
+let userScore = 0;
+let computerScore = 0;
+rock.addEventListener("click", function () {
+  let computerSelection = computerPlay();
+  // * Rock Cases
+  if (computerSelection === "Scissors") {
+    userScore = userScore + 1;
+    results.innerText = `You win! Computer picked ${computerSelection}`;
+  } else if (computerSelection === "Paper") {
+    computerScore = computerScore + 1;
+    results.innerText = `You lose! Computer picked ${computerSelection}`;
+  } else if (computerSelection === "Rock") {
+    results.innerText = `It's a tie!! Both of you picked ${computerSelection}`;
+  }
+});
 
-let playerSelection = prompt('Pick rock, paper, scissors')
+paper.addEventListener("click", function () {
+  let computerSelection = computerPlay();
+  // * Paper Cases
+  if (computerSelection === "Rock") {
+    userScore = userScore + 1;
+    results.innerText = `You win! Computer picked ${computerSelection}`;
+  } else if (computerSelection === "Scissors") {
+    computerScore = computerScore + 1;
+    results.innerText = `You lose! Computer picked ${computerSelection}`;
+  } else if (computerSelection === "Paper") {
+    results.innerText = `It's a tie!! Both of you picked ${computerSelection}`;
+  }
+});
 
-
-const playRound = (playerSelection, computerSelection) => {   
-
-    // * Rock Cases
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') {
-        userScore = userScore + 1 
-        return `You win! Computer picked ${computerSelection}`
-    } else if(playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
-        computerScore = computerScore + 1
-        return `You lose! Computer picked ${computerSelection}`
-    } else if(playerSelection.toLowerCase() === 'rock' && computerSelection === 'Rock'){
-        return `It's a tie!! Both of you picked ${computerSelection}`
-    }
-
-        // * Paper Cases
-    if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') {
-         userScore = userScore + 1 
-        return `You win! Computer picked ${computerSelection}`
-    } else if(playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
-        computerScore = computerScore + 1
-        return `You lose! Computer picked ${computerSelection}`
-    } else if(playerSelection.toLowerCase() === 'paper' && computerSelection === 'Paper'){
-        return `It's a tie!! Both of you picked ${computerSelection}`
-    }
-
-            // * Scissors Cases
-    if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') {
-         userScore = userScore + 1 
-        return `You win! Computer picked ${computerSelection}`
-    } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
-        computerScore = computerScore + 1
-        return `You lose! Computer picked ${computerSelection}`
-    } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Scissors'){
-        return `It's a tie!! Both of you picked ${computerSelection}`
-    }
-}
-
-const game = (playerSelection, computerSelection) =>{
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerPlay())) 
-        
-    }
-
-}
-
-console.log(game('rock', computerPlay()))
-console.log()
+scissors.addEventListener("click", function () {
+  let computerSelection = computerPlay();
+  // * Scissors Cases
+  if (computerSelection === "Paper") {
+    userScore = userScore + 1;
+    results.innerText = `You win! Computer picked ${computerSelection}`;
+  } else if (computerSelection === "Rock") {
+    computerScore = computerScore + 1;
+    results.innerText = `You lose! Computer picked ${computerSelection}`;
+  } else if (
+    
+    computerSelection === "Scissors"
+  ) {
+    results.innerText = `It's a tie!! Both of you picked ${computerSelection}`;
+  }
+});
